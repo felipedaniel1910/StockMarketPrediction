d5045979a9d7d0fa84ff429722bd977c5e088eb1
FelipeDaniel <felipedaniel1910@hotmail.com>
2023-12-17 15:17:27 -0300

    Extract Process Completion
    Extracting the data from Fundamentus with Selenium

---
+++ paths.py
@@ -0,0 +1,7 @@
+#some paths needed in the web scrapping part
+#source_path  = 'C:/Users/felip/Downloads'
+#destination_path  = 'C:/Users/felip/Documents/GitHub/StockMarketPrediction/balances'
+# paths.py
+class PathInfo:
+    source_path = 'C:/Users/felip/Downloads'
+    destination_path = 'C:/Users/felip/Documents/GitHub/StockMarketPrediction/balances'
