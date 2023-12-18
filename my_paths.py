fa7c4faba968c734c3e7949444e60841abcea537
FelipeDaniel <felipedaniel1910@hotmail.com>
2023-12-18 01:13:11 -0300

    Getting the data (web scraping)

---
+++ my_paths.py
@@ -0,0 +1,3 @@
+source_path_download = 'C:/Users/felip/Downloads'
+destination_path_zip = 'C:/Users/felip/Documents/GitHub/StockMarketPrediction/balances'
+destination_path_xls = 'C:/Users/felip/Documents/GitHub/StockMarketPrediction/balances/extracted'
--- paths.py
+++
@@ -1,7 +0,0 @@
-#some paths needed in the web scrapping part
-#source_path  = 'C:/Users/felip/Downloads'
-#destination_path  = 'C:/Users/felip/Documents/GitHub/StockMarketPrediction/balances'
-# paths.py
-class PathInfo:
-    source_path = 'C:/Users/felip/Downloads'
-    destination_path = 'C:/Users/felip/Documents/GitHub/StockMarketPrediction/balances'
