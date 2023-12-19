bcd68ccde455fd22537afd30fb6f03a47f220d58
FelipeDaniel <felipedaniel1910@hotmail.com>
2023-12-19 16:02:16 -0300

    Getting the cotations

--- my_paths.py
+++
@@ -1,3 +0,0 @@
-source_path_download = 'C:/Users/felip/Downloads'
-destination_path_zip = 'C:/Users/felip/Documents/GitHub/StockMarketPrediction/balances'
-destination_path_xls = 'C:/Users/felip/Documents/GitHub/StockMarketPrediction/balances/extracted'
